193ab655a5937a43f285a475d717911c8088cfa2
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-10-30 00:40:51 +0100

    Auto Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+module.exports = function(config,env) {
+      const Influx = require('influx');
+
+      let influx = null;
+
+      const _initInflux = async function() {
+        influx = new Influx.InfluxDB({
+              host: config.influxdb_host,
+              port: config.influxdb_port,
+              database: config.influxdb_database,
+              username: config.influxdb_username,
+              password: config.influxdb_password
+        });
+        return;
+      }
+
+      const archive = async function(json) {
+        if(typeof json.meterId == 'undefined') {
+          return { 'err': 'meterId missing' };
+        }
+        if((!Array.isArray(json.readings))||(json.readings.length==0)||(typeof json.readings[0].reading !== 'number')) {
+          return { 'err': 'readings missing or nan' };
+        }
+        if(typeof json.timeStamp !== 'number') {
+          json.timeStamp = new Date().getTime();
+        }
+        try {
+        if(influx == null) {
+            await _initInflux();
+        }
+        for(let i = 0; i< json.readings.length;i++) {
+          if(typeof json.readings[i].timeStamp == 'undefined') json.readings[i].timeStamp = new Date().getTime();
+
+          influx.writePoints([
+                {
+                  measurement: json.meterId,
+                  fields: { reading: json.readings[i].reading,time:json.readings[i].timeStamp}
+                }
+          ]);
+        }
+        } catch(e) {
+            return { 'err': e.message};
+        }
+        return { 'status': 'ok', 'err':null};
+      }
+
+      const addHandlers = function() {
+        let wwwroot = config.wwwroot || '';
+
+        env.app.get(wwwroot + '/',async function (req, res) {
+          let json = {};
+          let status = {};
+          let reading = {};
+          if(typeof req.query.timeStamp !== 'undefined') {
+            reading.timeStamp = req.query.timeStamp;
+          }
+          if(typeof req.query.reading !== 'undefined') {
+            reading.reading = req.query.reading * 1;
+          }
+          if(typeof req.query.meterId !== 'undefined') {
+            json.meterId = req.query.meterId;
+          }
+          json.readings = [];
+          json.readings.push(reading);
+          status = await archive(json);
+          res.send(status);
+        });
+        env.app.post(wwwroot + '/',async function (req, res) {
+            let json = req.body;
+            let status = {};
+            if(Array.isArray(json.readings)) {
+              status = await archive(json);
+            } else {
+              json.readings = [json.readings];
+              status = await archive(json);
+            }
+            res.send(status);
+        });
+      }
+      addHandlers();
+}
---
+++ influxdb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade -y
+
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt update && sudo apt install -y influxdb
+
+sudo systemctl unmask influxdb.service
+sudo systemctl start influxdb
+sudo systemctl enable influxdb.service
